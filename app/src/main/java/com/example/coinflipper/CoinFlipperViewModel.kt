58163105279d3b40e1200ffd7a30a56d4cca4c06
AS-Coope <deltazirconium@gmail.com>
2023-10-06 12:40:05 -0500

    Add flip function to CoinFlipperViewModel to determine Heads or Tails

--- app/src/main/java/com/example/coinflipper/CoinFlipperViewModel.kt
+++ app/src/main/java/com/example/coinflipper/CoinFlipperViewModel.kt
@@ -1,6 +1,17 @@
 package com.example.coinflipper
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class CoinFlipperViewModel: ViewModel() {
+
+    private var _coinFlipTextResult = MutableLiveData<String>()
+    val coinFlipTextResult: LiveData<String>
+            get() = _coinFlipTextResult
+
+    fun flip(): String{
+        _coinFlipTextResult.value = if ((1..2).random() == 1) "Heads" else "Tails"
+        return coinFlipTextResult.value!!
+    }
 }
