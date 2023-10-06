a877b787de5e8916792fb49d22149f34ce7e7587
AS-Coope <deltazirconium@gmail.com>
2023-10-06 12:11:38 -0500

    Add activity-ktx artifact in gradle to allow activity to reference view model via viewModels()

--- app/src/main/java/com/example/coinflipper/MainActivity.kt
+++ app/src/main/java/com/example/coinflipper/MainActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: CoinFlipperViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
